7b7ee2326dc5fd39f1b2979c38a082d3812f0140
lizehongss <13600484164@163.com>
2018-02-22 22:09:08 +0800

    添加vue-router

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './app.vue';
+Vue.use(VueRouter);
+
+const Routers =[
+{
+	path:'/index',
+	component: (resolve) => require(['./views/index.vue'],resolve)
+},
+{
+	path: '/about',
+	component:(resolve) => require(['./views/about.vue'],resolve)
+},
+{
+	path: '/user/:id',
+	component: (resolve) =>require(['./views/user.vue'],resolve)
+},
+{
+	path: '*',
+	redirect:'/index'
+}]
+const RouterConfig={
+	mode:'history',
+	routes: Routers
+};
+const router =new VueRouter(RouterConfig);
 new Vue({
 	el: '#app',
+	router: router,
 	render: h=>h(App)
 })
